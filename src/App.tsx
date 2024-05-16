4d84f0a97f51c0fd76acca612f41ce1c0ccebfc5
gabrielsouzaleite <gabriel.s.leite05@gmail.com>
2024-05-16 12:06:01 -0300

    NLW web project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/global.css'
+import './lib/dayjs'
+import { SummaryTable } from './components/SummaryTable';
+import { Header } from './components/Header';
+
+export function App() {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+
+        <Header />
+        <SummaryTable />
+        
+      </div>
+    </div>
+  );
+}
